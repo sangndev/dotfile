5d0f722f378f4a93be07745f7092a8e8883062e4
sangnd <sang.nguyen@navigosgroup.com>
2025-09-17 17:58:12 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
 NVIM_PATH="$HOME/.config/nvim"
+NVIM_CACHED_PATH="$HOME/.local/share/nvim"
 TMUX_PATH="$HOME/.tmux"
 TMUX_CONF_PATH="$HOME/.tmux.conf"
+BASHRC_PATH="$HOME/.bashrc"
 
-# cleanup() {
-#   if [[ -d "$NVIM_PATH" ]]; then
-#     echo "has nvim folder\n"
-#   fi
-#   if [[ -d "$TMUX_PATH" ]]; then
-#     echo "has tmux folder\n"
-#   fi
-#   if [[ -f "$TMUX_CONF_PATH" ]]; then
-#     echo "has tmux config file\n"
-#   fi
-# }
+# [ Handlers ]
+help() {
+  cat <<EOF
+\n
+These packages/tools here are needed for this dotfile to work perfectly\n\n
+1. Neovim (https://github.com/neovim/neovim/blob/master/INSTALL.md)\n
+2. Tmux (https://github.com/tmux/tmux/wiki/Installing)\n
+3. NerdFont (https://github.com/ryanoasis/nerd-fonts/blob/master/readme.md#font-installation)
+EOF
+}
 
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do 
   case $1 in
-    -c | --clean )
-      echo "cleanup"
+    -h | --help )
+      echo $(help)
       exit
       ;;
-    -s | --string )
-      shift
-      string=$1
+    -c | --clean )
+      echo $(clean)
+      exit
       ;;
-    -f | --flag )
-      flag=1
+    -u | --update )
+      echo "update"
+      exit
+      ;;
+    -i | --install )
+      echo "install"
+      exit
       ;;
     * )
       echo "Unknown option: $1"
@@ -41,8 +47,9 @@ if [[ "$1" == '--' ]]; then
 fi
 if [[ $# -eq 0 ]]; then
   echo "Usage: $0 [options]"
-  echo "  -c, --clean     cleanup"
-  echo "  -s, --string X  pass string"
-  echo "  -f, --flag      enable flag"
+  echo "  -h, --help     get a help for installing required packages/tools"
+  echo "  -i, --init     install needed packages and link config files/folder "
+  echo "  -u, --update   update config files/folder"
+  echo "  -c, --clean    delete all config files/folder"
   exit 1
 fi
