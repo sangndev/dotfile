a7d4308325992abf4531b573f2d65f0a3d698afc
sangnd <sang.nguyen@navigosgroup.com>
2025-10-13 14:14:20 +0700

    Update neovim config

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,32 +1,47 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { vim.fn.stdpath("data") .. '/mason/bin/vtsls', '--stdio' },
+  name = "vtsls",
+  cmd = { vim.fn.stdpath("data") .. "/mason/bin/vtsls", "--stdio" },
   init_options = {
-    hostInfo = 'neovim',
+    hostInfo = "neovim",
   },
+
   filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
   },
+
+  ---@param bufnr integer
   root_dir = function(bufnr, on_dir)
-    -- The project root is where the LSP can be started from
-    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
-    -- We select then from the project root, which is identified by the presence of a package
-    -- manager lock file.
-    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.lock' }
-    -- Give the root markers equal priority by wrapping them in a table
-    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers } or root_markers
-    local project_root = vim.fs.root(bufnr, root_markers)
-    if not project_root then
-      return
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+
+    -- Detect nearest project root
+    local root = vim.fs.root(bufnr, {
+      "tsconfig.json",
+      "jsconfig.json",
+      "package.json",
+      "package-lock.json",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+      "bun.lockb",
+      "bun.lock",
+      "deno.lock",
+    })
+
+    -- Fallback: file's directory
+    if not root or root == "" then
+      root = vim.fs.dirname(fname)
     end
 
-    on_dir(project_root)
+    if root then
+      on_dir(root)
+    end
   end,
+
   commands = {
     OrganizeImports = {
       function()
