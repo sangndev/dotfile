22560ca31a83750a8eee236ac08932647c0342fa
sangnd <sang.nguyen@navigosgroup.com>
2026-05-29 10:06:19 +0700

    refactor nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -175,16 +175,47 @@ do
 	require("vim._core.ui2").enable({
 		enable = true,
 		msg = {
-			target = "msg",
-			timeout = 2000,
 			targets = {
 				[""] = "msg",
+				empty = "cmd",
+				bufwrite = "msg",
+				confirm = "cmd",
+				emsg = "pager",
 				echo = "msg",
 				echomsg = "msg",
+				echoerr = "pager",
+				completion = "cmd",
+				list_cmd = "pager",
+				lua_error = "pager",
 				lua_print = "msg",
-				cmd = "cmd",
+				progress = "pager",
+				rpc_error = "pager",
+				quickfix = "msg",
+				search_cmd = "cmd",
+				search_count = "cmd",
+				shell_cmd = "pager",
+				shell_err = "pager",
+				shell_out = "pager",
+				shell_ret = "msg",
+				undo = "msg",
+				verbose = "pager",
+				wildlist = "cmd",
+				wmsg = "msg",
+				typed_cmd = "cmd",
 			},
-			border = "rounded",
+			cmd = {
+				height = 0.5,
+			},
+			dialog = {
+				height = 0.5,
+			},
+			msg = {
+				height = 0.3,
+				timeout = 5000,
+			},
+			pager = {
+				height = 0.5,
+			},
 		},
 	})
 
